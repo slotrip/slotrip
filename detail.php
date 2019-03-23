3adbc7d8f5525ae090ab7c55c11175aa1cea1628
m <mell.mmm.1022@gmail.com>
2019-03-23 18:45:01 +0900

    commit

---
+++ detail.php
@@ -0,0 +1,45 @@
+<?
+
+require_once("./db.inc");
+
+$list_id = $_POST['list_id'];
+$list_id = 1;
+
+try{
+
+    //一覧データ取得+詳細
+	$sql = 'SELECT list_id,title, catch_text, key_id, prefecture, address, address_desc,description,note FROM LIST WHERE list_id=?';
+	$stmt = $dbh->prepare($sql);
+	$data[] = $list_id;
+	$stmt->execute($data);
+
+
+
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $detail_array = array();
+	$detail_array = $rec['name'];
+	$detail_array['list_id'] = $rec['list_id'];
+	$detail_array['title'] = $rec['title'];
+	$detail_array['key_id'] = $rec['key_id'];
+	$detail_array['prefecture'] = $rec['prefecture'];
+	$detail_array['address'] = $rec['address'];
+	$detail_array['address_desc'] = $rec['address_desc'];
+	$detail_array['description'] = $rec['description'];
+	$detail_array['note'] = $rec['note'];
+
+	$dbh = null; //DB切断
+
+}catch(Exception $e){
+	echo 'エラー';
+	exit();
+}
+
+
+
+
+//詳細データ
+print_r($detail_array);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,79 @@
+<?
+
+require_once("./db.inc");
+
+try{
+
+    //キーワードデータ取得
+	$sql = 'SELECT key_id,keyword FROM KEYWORD WHERE 1 ORDER BY key_id'; //1は全てという意味
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute();
+
+    $key_array = array();
+    $num = 1;
+	while(true){
+		$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		if($rec == false){
+			break;
+		}
+
+        $tmp_arr['key_id'] = $rec['key_id'];
+        $tmp_arr['keyword'] = $rec['keyword'];
+        $key_array[$num ] = $tmp_arr;
+        $num++;
+
+	}
+
+
+
+    //一覧データ取得
+	$sql = 'SELECT list_id,title, catch_text, key_id, prefecture, address, address_desc FROM LIST WHERE 1 ORDER BY list_id'; //1は全てという意味
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute();
+
+    $list_array = array();
+    $num = 1;
+	while(true){
+		$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		if($rec == false){
+			break;
+		}
+
+        $tmp_arr['list_id'] = $rec['list_id'];
+        $tmp_arr['title'] = $rec['title'];
+        $tmp_arr['key_id'] = $rec['key_id'];
+        $tmp_arr['prefecture'] = $rec['prefecture'];
+        $tmp_arr['address'] = $rec['address'];
+        $tmp_arr['address_desc'] = $rec['address_desc'];
+        $list_array[$num ] = $tmp_arr;
+        $num++;
+
+	}
+
+	$dbh = null; //DB切断
+
+
+}catch(Exception $e){
+	echo 'エラー';
+	exit();
+}
+
+
+
+//キーワード配列
+//print_r($key_array);
+
+//js用の値
+$key_json = json_encode($key_array);
+print_r($key_json);
+
+//一覧配列
+//print_r($list_array);
+
+
+//テンプレート
+require_once("./example1.html")
+
+?>
